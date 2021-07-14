d9bdd11969c7875bb54ba2c9088bb8569120a7cc
Brandon Akers <brandonakers729@gmail.com>
2021-07-14 17:21:50 -0500

    fixed route issue

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,14 +7,14 @@ router.get("/", async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
-      include: [
-        {
-          model: Product,
-          attributes: ["product_name"],
-          as: "products_in_category",
-        },
-      ],
+      include: [{ model: Product }],
     });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category exists for this id!" });
+      return;
+    }
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -25,13 +25,7 @@ router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [
-        {
-          model: Product,
-          attributes: ["product_name"],
-          as: "products_in_category",
-        },
-      ],
+      include: [{ model: Product }],
     });
 
     if (!categoryData) {
@@ -48,8 +42,9 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new category
   try {
-    const newCategory = await Category.create(req.body);
-    res.status(200).json(newCategory);
+    const categoryData = await Category.create(req.body);
+
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -63,10 +58,12 @@ router.put("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
+
     if (!categoryData[0]) {
       res.status(404).json({ message: "No category exists for this id!" });
       return;
     }
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -81,13 +78,16 @@ router.delete("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
+
     if (!categoryData) {
       res.status(404).json({ message: "No category exists for this id!" });
       return;
     }
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,6 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // get all products
 router.get("/", async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
@@ -20,17 +19,14 @@ router.get("/", async (req, res) => {
 // get one product
 router.get("/:id", async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
-
     if (!productData) {
       res.status(404).json({ message: "No product exists for this id!" });
       return;
     }
-
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -107,7 +103,6 @@ router.put("/:id", (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
